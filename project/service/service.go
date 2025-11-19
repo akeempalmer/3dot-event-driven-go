b956b7bfd38b83342dab4cb74793276a650b8d37
Akeem Palmer <akeemk.palmer@gmail.com>
2025-11-18 23:37:14 -0500

    Completed: 02-async (03-project-async)

---
+++ project/service/service.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"context"
+	"errors"
+	stdHTTP "net/http"
+
+	"github.com/labstack/echo/v4"
+
+	ticketsHttp "tickets/http"
+	"tickets/worker"
+)
+
+type Service struct {
+	echoRouter *echo.Echo
+}
+
+func New(
+	spreadsheetsAPI worker.SpreadsheetsAPI,
+	receiptsService worker.ReceiptsService,
+) Service {
+	worker := worker.NewWorker(spreadsheetsAPI, receiptsService)
+	echoRouter := ticketsHttp.NewHttpRouter(worker)
+
+	go worker.Run(context.Background())
+	return Service{
+		echoRouter: echoRouter,
+	}
+}
+
+func (s Service) Run(ctx context.Context) error {
+	err := s.echoRouter.Start(":8080")
+	if err != nil && !errors.Is(err, stdHTTP.ErrServerClosed) {
+		return err
+	}
+
+	return nil
+}
