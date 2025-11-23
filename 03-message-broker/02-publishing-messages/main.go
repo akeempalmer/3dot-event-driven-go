e51042436132625dded227ecd5ba7b849f641701
Akeem Palmer <akeemk.palmer@gmail.com>
2025-11-23 12:23:42 -0500

    Completed: 03-MessageBroker_02-PublishingMessages

---
+++ 03-message-broker/02-publishing-messages/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	logger := watermill.NewSlogLogger(nil)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
+
+	publisher, err := redisstream.NewPublisher(redisstream.PublisherConfig{
+		Client: rdb,
+	}, logger)
+	if err != nil {
+		logger.Error("failed to create publisher", err, nil)
+	}
+
+	msg := message.NewMessage(watermill.NewUUID(), []byte("50"))
+	msg2 := message.NewMessage(watermill.NewUUID(), []byte("100"))
+
+	err = publisher.Publish("progress", msg)
+	if err != nil {
+		logger.Error("failed to publish message", err, nil)
+	}
+
+	err = publisher.Publish("progress", msg2)
+	if err != nil {
+		logger.Error("failed to publish message", err, nil)
+	}
+
+}
