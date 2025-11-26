f066b10bde25e493190a7df8b77b6249f2f6e6f2
Akeem Palmer <akeemk.palmer@gmail.com>
2025-11-25 23:48:33 -0500

    Completed: 04-router_01-handlers

---
+++ 04-router/02-no-publisher-handlers/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	logger := watermill.NewSlogLogger(nil)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
+
+	sub, err := redisstream.NewSubscriber(redisstream.SubscriberConfig{
+		Client: rdb,
+	}, logger)
+	if err != nil {
+		panic(err)
+	}
+}
