692beb1775c837ec81bf8d090476a0e1d2335124
Akeem Palmer <akeemk.palmer@gmail.com>
2025-12-13 20:28:30 -0500

    Completed 07-errors_01-project-error-logging

--- project/middlewares/logger.go
+++ project/middlewares/logger.go
@@ -15,13 +15,19 @@ type LogHeader struct {
 }
 
 func (lh LogHeader) LoggerMiddleware(next message.HandlerFunc) message.HandlerFunc {
-	return func(msg *message.Message) ([]*message.Message, error) {
+	return func(msg *message.Message) (msgs []*message.Message, err error) {
 		logger := log.FromContext(msg.Context())
 		logger.With("message_id", msg.UUID,
 			"metadata", msg.Metadata,
 			"handler", msg.Context())
 
 		logger.With("message_id", log.CorrelationIDFromContext(msg.Context())).Info("Handling a message")
+
+		defer func() {
+			if err != nil {
+				logger.With("error", err, "message_id", msg.UUID).Error("Error while handling a message")
+			}
+		}()
 
 		return next(msg)
 	}
